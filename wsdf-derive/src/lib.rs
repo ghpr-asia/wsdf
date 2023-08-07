108be6e96e91b7914f135852ad0c189bc5ef7303
junyi <47441677+immanuelhume@users.noreply.github.com>
2023-08-08 00:40:17 +0800

    Add top level impl for Proto

--- wsdf-derive/src/lib.rs
+++ wsdf-derive/src/lib.rs
@@ -457,3 +457,208 @@ fn derive_dissect_impl_enum(
         }
     }
 }
+
+struct SelectedProtocols(Vec<syn::Ident>);
+
+impl Parse for SelectedProtocols {
+    fn parse(input: ParseStream) -> syn::Result<Self> {
+        let roots: Vec<syn::Ident> = input
+            .parse_terminated(syn::Ident::parse, syn::Token![,])?
+            .into_iter()
+            .collect();
+        Ok(SelectedProtocols(roots))
+    }
+}
+
+#[proc_macro]
+pub fn protocol(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as SelectedProtocols);
+
+    let register_protos = input.0.iter().flat_map(|ty| -> Vec<syn::Stmt> {
+        parse_quote! {
+            static mut PLUG: wsdf::epan_sys::proto_plugin = wsdf::epan_sys::proto_plugin {
+                register_protoinfo: None,
+                register_handoff: None,
+            };
+            // SAFETY: this code is only called once in a single thread when wireshark starts
+            unsafe {
+                PLUG.register_protoinfo =
+                    std::option::Option::Some(<#ty as wsdf::Proto>::register_protoinfo);
+                PLUG.register_handoff =
+                    std::option::Option::Some(<#ty as wsdf::Proto>::register_handoff);
+                wsdf::epan_sys::proto_register_plugin(&PLUG);
+            }
+        }
+    });
+
+    quote! {
+        thread_local! {
+            static __WSDF_HF_INDICES: std::cell::RefCell<wsdf::HfIndices> = wsdf::HfIndices::default().into();
+            static __WSDF_ETT_INDICES: std::cell::RefCell<wsdf::EttIndices> = wsdf::EttIndices::default().into();
+            static __WSDF_DTABLES: std::cell::RefCell<wsdf::DissectorTables> = wsdf::DissectorTables::default().into();
+        }
+
+        #[no_mangle]
+        extern "C" fn plugin_register() {
+            #(#register_protos)*
+        }
+    }.into()
+}
+
+#[proc_macro_derive(Proto, attributes(wsdf))]
+pub fn derive_proto(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input!(input as syn::DeriveInput);
+    let ret = derive_proto_impl(&input)
+        .map(|x| x.to_token_stream())
+        .unwrap_or_else(|e| e.to_compile_error());
+    ret.into()
+}
+
+fn derive_proto_impl(input: &syn::DeriveInput) -> syn::Result<syn::ItemImpl> {
+    if !matches!(input.data, syn::Data::Struct { .. }) {
+        return make_err(&input, "only structs can derive Proto");
+    }
+
+    let ident = &input.ident;
+
+    let proto_opts = init_options::<ProtocolOptions>(&input.attrs)?;
+    if proto_opts.decode_from.is_empty() {
+        return make_err(
+            &input.ident,
+            "expected some way of registering with dissector table",
+        );
+    }
+
+    let add_dissector = proto_opts.decode_from.iter().map(DecodeFrom::to_tokens);
+
+    let upper_cased = input.ident.to_wsdf_upper_case();
+    let snake_cased = input.ident.to_wsdf_snake_case();
+
+    let proto_desc = proto_opts.proto_desc.as_ref().unwrap_or(&upper_cased);
+    let proto_name = proto_opts.proto_name.as_ref().unwrap_or(&upper_cased);
+    let proto_filter = proto_opts.proto_filter.as_ref().unwrap_or(&snake_cased);
+
+    let proto_desc_cstr: syn::Expr = cstr!(proto_desc);
+    let proto_name_cstr: syn::Expr = cstr!(proto_name);
+    let proto_filter_cstr: syn::Expr = cstr!(proto_filter);
+
+    Ok(parse_quote! {
+        impl wsdf::Proto for #ident {
+            #[allow(clippy::missing_safety_doc)]
+            unsafe extern "C" fn dissect_main(
+                tvb: *mut epan_sys::tvbuff,
+                pinfo: *mut epan_sys::_packet_info,
+                tree: *mut epan_sys::_proto_node,
+                data: *mut std::ffi::c_void,
+            ) -> std::ffi::c_int {
+                // Clear columns
+                wsdf::epan_sys::col_set_str(
+                    (*pinfo).cinfo,
+                    wsdf::epan_sys::COL_PROTOCOL as std::ffi::c_int,
+                    #proto_desc_cstr,
+                );
+                wsdf::epan_sys::col_clear(
+                    (*pinfo).cinfo,
+                    wsdf::epan_sys::COL_INFO as std::ffi::c_int,
+                );
+
+                // Initialize rust-owned TVB
+                let tvb_len = unsafe {
+                    wsdf::epan_sys::tvb_reported_length(tvb) as usize
+                };
+                let mut tvb_buf = Vec::new();
+                tvb_buf.resize(tvb_len, 0);
+                unsafe {
+                    wsdf::epan_sys::tvb_memcpy(
+                        tvb,
+                        tvb_buf.as_mut_ptr() as *mut std::ffi::c_void,
+                        0,
+                        tvb_len,
+                    );
+                }
+
+                __WSDF_HF_INDICES.with(|hf_indices|
+                    __WSDF_ETT_INDICES.with(|etts|
+                        __WSDF_DTABLES.with(|dtables| {
+                            let hf_indices = hf_indices.borrow();
+                            let etts = etts.borrow();
+                            let dtables = dtables.borrow();
+                            let mut fields = wsdf::FieldsStore::default();
+                            let args = wsdf::DissectorArgs {
+                                hf_indices: &hf_indices,
+                                etts: &etts,
+                                dtables: &dtables,
+                                tvb,
+                                pinfo,
+                                proto_root: tree,
+                                data: &tvb_buf,
+                                prefix: #proto_filter,
+                                prefix_local: #proto_filter,
+                                offset: 0,
+                                parent: tree,
+                                variant: std::option::Option::None,
+                                list_len: std::option::Option::None,
+                                ws_enc: std::option::Option::None,
+                            };
+
+                            <#ident as Dissect<'_, ()>>::add_to_tree(&args, &mut fields) as _
+                        })
+                    )
+                )
+            }
+
+            unsafe extern "C" fn register_protoinfo() {
+                let proto_id = unsafe {
+                    wsdf::epan_sys::proto_register_protocol(
+                        #proto_desc_cstr,
+                        #proto_name_cstr,
+                        #proto_filter_cstr,
+                    )
+                };
+
+
+                __WSDF_HF_INDICES.with(|hf_indices|
+                    __WSDF_ETT_INDICES.with(|etts|
+                        __WSDF_DTABLES.with(|dtables| {
+                            let mut hf = hf_indices.borrow_mut();
+                            let mut ett = etts.borrow_mut();
+                            let mut dtable = dtables.borrow_mut();
+                            let mut ws_indices = wsdf::WsIndices {
+                                hf: &mut hf,
+                                ett: &mut ett,
+                                dtable: &mut dtable,
+                            };
+
+                            ws_indices.hf.insert(#proto_filter, proto_id);
+
+                            let args = wsdf::RegisterArgs {
+                                proto_id,
+                                name: #proto_name_cstr,
+                                prefix: #proto_filter,
+                                blurb: std::ptr::null(), // @todo
+                                ws_type: std::option::Option::None,
+                                ws_display: std::option::Option::None,
+                            };
+
+                            <#ident as Dissect<'_, ()>>::register(&args, &mut ws_indices);
+                        })
+                    )
+                );
+            }
+
+            unsafe extern "C" fn register_handoff() {
+                __WSDF_HF_INDICES.with(|hf_indices| {
+                    let hf_indices = hf_indices.borrow();
+                    let proto_id = hf_indices.get(#proto_filter).unwrap();
+                    unsafe {
+                        let handle = wsdf::epan_sys::create_dissector_handle(
+                            std::option::Option::Some(<#ident as wsdf::Proto>::dissect_main),
+                            proto_id,
+                        );
+                        #(#add_dissector)*
+                    }
+                });
+            }
+        }
+    })
+}
